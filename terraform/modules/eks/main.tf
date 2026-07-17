98c0dd243e6b5e04eb22f691353aaeb30cc6ccb8
Rukiyath Faheema <Rukiyath.Faheema@mondiait.com>
2026-07-17 16:27:00 +0400

    fix: restore eks module content

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,1 +1,72 @@
+resource "aws_eks_cluster" "this" {
+  name     = "${var.cluster_name}-${var.environment}"
+  role_arn = var.cluster_role_arn
+  version  = var.kubernetes_version
 
+  vpc_config {
+    subnet_ids              = var.subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    security_group_ids      = [aws_security_group.cluster.id]
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-${var.environment}"
+  }
+}
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.cluster_name}-nodes-${var.environment}"
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.subnet_ids
+
+  instance_types = var.node_instance_types
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    min_size     = var.node_min_size
+    max_size     = var.node_max_size
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-nodes-${var.environment}"
+  }
+}
+
+resource "aws_security_group" "cluster" {
+  name_prefix = "${var.cluster_name}-cluster-sg-"
+  vpc_id      = var.vpc_id
+  description = "Controls network access to ${var.cluster_name} cluster"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-cluster-sg-${var.environment}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "allow_cross_vpc" {
+  count = length(var.allowed_cidr_blocks) > 0 ? 1 : 0
+
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = var.allowed_cidr_blocks
+  security_group_id = aws_security_group.cluster.id
+  description       = "Allow traffic from peered VPC"
+}
